97271bd1ed2c18636058ecb1bff4f14519a934f5
Mario Gomes Neves Neto <mario.neves@petrobras.com.br>
2019-11-20 14:00:44 -0300

    render implemented

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ print(env.info)
 for i_episode in range(1):
     observation = env.reset()
     print('initial observation: {}'.format(observation))
-    for t in range(10):
-        # env.render()
+    for t in range(1000):
+        env.render()
         action = env.action_space.sample()
         observation, reward, done, step_info = env.step(action)
-        print('action: {}, observation: {}'.format(action, observation))
+        #print('action: {}, observation: {}'.format(action, observation))
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
-# env.close()
+env.close()
